f98a3503895148dafe79dcdacb68f84e1474ad2b
mike2477 <mthomas.denver@gmail.com>
2021-05-24 08:26:30 -0600

    new capacity graphs added

---
+++ js/animated-line.js
@@ -0,0 +1,103 @@
+async function drawChartArea() {
+
+  const emissionsDataset = await d3.csv("../data/us-historical-coal-emissions.csv", d3.autoType)
+
+  const totalEmissions = d => d.emissions
+  const dateParser = d3.timeParse("%Y")
+  const year = d => dateParser(d.year)
+
+  const divElement = document.getElementById('annual-emissions-wrapper')
+  const divWidth = divElement.offsetWidth
+
+  let dimensions = {
+    width: divWidth,
+    height: window.innerHeight * 0.75,
+    margin: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  }
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom
+
+  const xScale = d3.scaleTime()
+      .domain(d3.extent(emissionsDataset, year))
+      .range([0, dimensions.boundedWidth])
+
+  // console.log()
+
+  const yScale = d3.scaleLinear()
+      .domain([500000000,2500000000])
+      .range([dimensions.boundedHeight, 0])
+
+  const lineGenerator = d3.line()
+      .x(d => xScale(year(d)))
+      .y(d => yScale(totalEmissions(d)))
+
+  const wrapper = d3.select("#annual-emissions-wrapper")
+    .append("svg")
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
+
+  const bounds = wrapper.append("g")
+      .style("transform", `translate(${
+        dimensions.margin.left
+      }px, ${
+        dimensions.margin.top
+      }px)`)
+
+  // Draw axis
+
+  const yAxisGenerator = d3.axisLeft()
+    .scale(yScale)
+    .tickFormat(d3.format(".2s"))
+
+  const yAxis = bounds.append("g")
+    .call(yAxisGenerator)
+
+  const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+
+  const xAxis = bounds.append("g")
+    .call(xAxisGenerator)
+      .style("transform", `translateY(${
+        dimensions.boundedHeight
+      }px)`)
+
+
+  function animateLine() {
+    const line = bounds.append("path")
+        .attr("d", lineGenerator(emissionsDataset))
+        .attr("fill", "none")
+        .attr("stroke", "#2F394B")
+        .attr("stroke-width", 2)
+
+    const totalLength = line.node().getTotalLength()
+
+    line
+      .attr("stroke-dasharray", totalLength + " " + totalLength)
+      .attr("stroke-dashoffset", totalLength)
+      .transition()
+        .duration(10000)
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0)
+  }
+
+  enterView({
+    selector: '#annual-emissions-wrapper',
+    enter: function(el) {
+      animateLine()
+    },
+    offset: 0.5,
+    once: true,
+  });
+
+}
+
+drawChartArea()
--- js/top-25-plants.js
+++ js/top-25-plants.js
@@ -1,41 +1,23 @@
 async function drawMap() {
 
   // Access data
-  const plantDataset = await d3.csv("../data/power-plant-data.csv", d3.autoType)
+  const plantDataset = await d3.csv("../data/top-25-plants.csv", d3.autoType)
   const usMapJson = await d3.json("../data/us-map-geojson.json")
-  const mineDataset = await d3.csv("../data/biggest-coal-mines.csv", d3.autoType)
+
 
   // plant accessors and filters
   const plantLat = d => d.latitude
   const plantLong = d => d.longitude
   const plantGeneration = d => d.generation_mwh
   const plantEmissions = d => d.emissions_tons
-  const plantFuelType = d => d.fuel_type
+  const retirementDate = d => d.retirement_date
+  const retirementStatus = d => d.retirement_status
 
-  // Plant filter criteria
-  const fuel = "COAL"
-  const topPlantFilter = 5
-
-  // Functions to filter power plants
-  const sortedPlants = plantDataset.slice().sort(function(a,b){
-    return plantEmissions(b)-plantEmissions(a)
-  })
-  const filterByFuel = sortedPlants.filter(d => d.fuel_type == fuel);
-  const plantsToMap = filterByFuel.splice(0, topPlantFilter)
-
-
-  // mine accessors
-  const mineLat = d => d.lat
-  const mineLong = d => d.lon
-  const mineProduction = d => d.production
-
-  const topMineFilter = 5
-  const sortedMines = mineDataset.slice().sort(function(a,b){
-    return mineProduction(b)-mineProduction(a)
-  })
-  const minesToMap = sortedMines.splice(0, topMineFilter)
 
   // Create bounds for the map to go in as usual
+  const divElement = document.getElementById('map-wrapper')
+  const divWidth = divElement.offsetWidth
+
   let dimensions = {
     width: window.innerWidth * 0.75,
     height: 500,
@@ -44,6 +26,19 @@ async function drawMap() {
   // Set projection as Albers and create Path Generator function
   const projection = d3.geoAlbers() // tells d3 to use Albers projection
   const pathGenerator = d3.geoPath(projection); // creates a function to convert lat and long to path
+
+  // Color scale for status of plant
+  function colorChange (d) {
+    let color = "white"
+    if (retirementStatus(d) == "Planned") // Yellow-ish color
+      color = "#ffa59e"
+    // else if (retirementStatus(d) == "Partial Closure Planned") // Orange-ish color
+    //   color = "#f77f00"
+    else
+      color = "#93003a" // red-ish color for "No Retirement Date"
+    return color
+  }
+
 
   //Create SVG element and append US map to the SVG
   const wrapper = d3.select("#map-wrapper")
@@ -61,12 +56,8 @@ async function drawMap() {
 
   // Scales and geo converter
   const plantScale = d3.scaleLinear()
-      .domain([1000000,25000000])
-      .range([1,25])
-
-  const mineScale = d3.scaleLinear()
-      .domain([5000000, 100000000])
-      .range([1,25])
+      .domain([10000000,25000000])
+      .range([10,25])
 
   function plantProjection (d) {   // create x, y coordinates for dot based on lat and long
     return "translate("
@@ -74,33 +65,29 @@ async function drawMap() {
     + ")"
   }
 
-  function mineProjection (d) {   // create x, y coordinates for dot based on lat and long
-    return "translate("
-    + projection([mineLong(d),mineLat(d)])
-    + ")"
-  }
-
-
   // Create dots for each power plant and scale
   const plantGroup = wrapper.append("g")
   const plantDots = plantGroup.selectAll("#circles")
-    .data(plantsToMap)
+    .data(plantDataset)
     .enter().append("circle")
-      .attr("r", 5)
+      .attr("r", d => plantScale(plantEmissions(d)))
       .attr("transform", d => plantProjection(d))
-      .attr("fill", "#2F394B")
+      .attr("fill", "#5681b9")
       .attr("opacity", 0.7)
 
-  // Create dots for each mine and scale
-  const mineGroup = wrapper.append("g")
-  const mineDots = mineGroup.selectAll("#circles")
-    .data(minesToMap)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("transform", d => mineProjection(d))
-      .attr("fill", "#E56B6F")
-      .attr("opacity", 0.7)
+  function changeColor() {
+    plantDots.transition()
+        .attr("fill", d => colorChange(d))
+  }
 
+  enterView({
+    selector: '.first-step',
+    enter: function(el) {
+      changeColor()
+    },
+    offset: 0.5,
+    once: true,
+  });
 
 }
 
